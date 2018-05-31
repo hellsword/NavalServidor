8beb5f7911a697f114a7c78bfcf8240f24b61c2d
SujetoAnonimo <pegajoso3000@gmail.com>
2018-05-31 18:04:46 -0400

    hgjg

---
+++ Naval_servidor/embarcacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naval_servidor
+{
+    class embarcacion
+    {
+        public string nombre { get; set; }
+        public int inicio { get; set; }
+        public int fin { get; set; }
+        public string estado { get; set; }
+    }
+}
